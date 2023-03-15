6c131f04d88b66b3c2e280b69dbde16d4d78f595
Burak Ocak <ocakbura@gmail.com>
2023-03-15 22:50:42 +0300

    feat: React ile UI oluşturulması tekrarlandı.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Section from "./Section.jsx";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <section>
-    <p>App 1</p>
-  </section>
-);
+
+  <Section />
+
+  );
 
